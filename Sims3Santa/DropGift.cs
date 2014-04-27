c0a3d84229840ab44d3037c42657046c9439a825
recursor94 <werdnaonaps@gmail.com>
2014-04-27 13:46:20 -0400

    created spawning function to spawn teddy bears

--- Sims3Santa/DropGift.cs
+++ Sims3Santa/DropGift.cs
@@ -16,8 +16,7 @@ namespace Sims3.Gameplay.Services.Recursor94
     public sealed class DropGift : Interaction<Sim, GameObject> //can't make it specific to my particular object for some reason.
     {
         public static readonly InteractionDefinition Singleton = new Definition();
-        private IGameObject giftBox;  //may change this to collection later, to add multiple gifts
-
+       
         private sealed class Definition : InteractionDefinition<Sim, GameObject, DropGift> 
         {
             protected override bool Test(Sim actor, GameObject target, bool isAutonomous, ref SimIFace.GreyedOutTooltipCallback greyedOutTooltipCallback)
@@ -35,23 +34,31 @@ namespace Sims3.Gameplay.Services.Recursor94
         protected override bool Run()
         {
             base.Actor.RouteToObjectRadiusAndCheckInUse(base.Target, 0.03f);
-            giftBox = GlobalFunctions.CreateObjectOutOfWorld("giftPartyBox"); //not sure if this will work.  Use Mailbox.putmail bottom of Run() method for reference.
-            //CarrySystem.PickUp(base.Actor, this.giftBox);  Can't use carrymanager not ICarryable
-            //use giftpile.add as reference
-
             base.EnterStateMachine("giftpile", "EnterDrop", "x");  
-           // this.mCurrentStateMachine.SetPropActor("gift", giftBox.ObjectId); crashes
             this.mCurrentStateMachine.SetPropActor("gift", ObjectGuid.InvalidObjectGuid); //works perfedtly. The solution was to set the actor to an invalid GUID.  Because sure.  why not.
             base.AnimateSim("ExitDrop");
             base.StandardExit();
-
-            Vector3 objPosition = base.Target.Position;
-            Vector3 giftPosition = new Vector3(objPosition.x + 0.06f, objPosition.y, objPosition.z);
-            giftBox.SetPosition(giftPosition);
-            GiftGiverSituation.debugMessage(giftBox.Position + "");
-
+        
+            spawnGifts(base.Actor.LotCurrent.GetSimsCount());
 
             return true;
+        }
+
+        public void spawnGifts(int numberOfSims)  //maybe in the future make it based on age, tunable variable for whether all age groups are okay or not.
+        {
+            IGameObject gift;
+            
+            float xOffset = 1f;
+            for (int n = 0; n < numberOfSims; n++)
+            {
+                gift = GlobalFunctions.CreateObjectOutOfWorld("TeddyBear"); //make random later.
+                Vector3 objPosition = base.Target.Position;
+                Vector3 giftPosition = new Vector3(objPosition.x + xOffset, objPosition.y, objPosition.z);
+                gift.AddToWorld();
+                gift.SetPosition(giftPosition);
+                GiftGiverSituation.debugMessage("Gift Position" + gift.Position + "santa object position" + this.Target.Position);
+                xOffset += 0.7f;
+            }
         }
     }
 }
